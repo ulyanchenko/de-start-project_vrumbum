63a96ef6a2b63f678358f57e574ccdb93423c878
ulyanchenko <daniil.mctracher@yandex.ru>
2024-10-28 18:24:06 +0300

    Update DDL.sql

--- DDL.sql
+++ DDL.sql
@@ -1,22 +1,47 @@
 /*Добавьте в этот файл все запросы, для создания схемы данных автосалона и
  таблиц в ней в нужном порядке*/
-CREATE SCHEMA IF NOT EXISTS raw_data;
-CREATE TABLE IF NOT EXISTS raw_data.sales(
+CREATE SCHEMA IF NOT EXISTS car_shop;
+
+CREATE TABLE IF NOT EXISTS car_shop.makes (
   id SERIAL PRIMARY KEY, 
-  auto VARCHAR(255), 
-  gasoline_consumption NUMERIC(4, 2), 
-  price NUMERIC(15, 2), 
-  sale_date DATE, 
-  person_name VARCHAR(255), 
-  phone VARCHAR(50), 
-  discount INTEGER, 
-  brand_origin VARCHAR(100)
+  name VARCHAR(255) NOT NULL UNIQUE
 );
 
-COPY raw_data.sales (
-  auto, gasoline_consumption, price, 
-  sale_date, person_name, phone, discount, 
-  brand_origin
-) 
-FROM 
-  'C:/Dev/cars.csv' DELIMITER ',' CSV HEADER;
+CREATE TABLE IF NOT EXISTS car_shop.car_models (
+  id SERIAL PRIMARY KEY, 
+  name VARCHAR(255) NOT NULL UNIQUE, 
+  make_id INT NOT NULL, 
+  FOREIGN KEY (make_id) REFERENCES car_shop.makes(id)
+);
+
+CREATE TABLE IF NOT EXISTS car_shop.colors (
+  id SERIAL PRIMARY KEY, 
+  color VARCHAR(255) NOT NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS car_shop.cars (
+  id SERIAL PRIMARY KEY, 
+  model_id INT NOT NULL, 
+  gasoline_consumption DECIMAL(5, 2), 
+  brand_origin VARCHAR(255), 
+  color_id INT NOT NULL, 
+  FOREIGN KEY (model_id) REFERENCES car_shop.car_models(id), 
+  FOREIGN KEY (color_id) REFERENCES car_shop.colors(id)
+);
+
+CREATE TABLE IF NOT EXISTS car_shop.customers (
+  id SERIAL PRIMARY KEY, 
+  person_name VARCHAR(255), 
+  phone VARCHAR(50)
+);
+
+CREATE TABLE IF NOT EXISTS car_shop.sales (
+  id SERIAL PRIMARY KEY, 
+  car_id INT NOT NULL, 
+  customer_id INT NOT NULL, 
+  sale_date DATE, 
+  price DECIMAL(10, 2), 
+  discount DECIMAL(5, 2) DEFAULT 0, 
+  FOREIGN KEY (car_id) REFERENCES car_shop.cars(id), 
+  FOREIGN KEY (customer_id) REFERENCES car_shop.customers(id)
+);
