9f0c8a7d146da4ddff9abc6a42f0b14517d9e4ec
ulyanchenko <daniil.mctracher@yandex.ru>
2024-10-28 17:30:52 +0300

    Update task_3.sql

--- task_3.sql
+++ task_3.sql
@@ -1,1 +1,13 @@
 /*добавьте сюда запрос для решения задания 3*/
+SELECT 
+    EXTRACT(MONTH FROM s.sale_date) AS month,
+    EXTRACT(YEAR FROM s.sale_date) AS year,
+    ROUND(AVG(s.price * (1 - s.discount / 100)), 2) AS price_avg
+FROM 
+    car_shop.sales s
+WHERE 
+    EXTRACT(YEAR FROM s.sale_date) = 2022
+GROUP BY 
+    month, year
+ORDER BY 
+    month;
