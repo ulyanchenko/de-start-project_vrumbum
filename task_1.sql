ee870eb9b48b63d59bd761f102e5aba6fdd042e6
ulyanchenko <daniil.mctracher@yandex.ru>
2024-10-28 17:31:50 +0300

    Update task_1.sql

--- task_1.sql
+++ task_1.sql
@@ -1,1 +1,10 @@
 /*добавьте сюда запрос для решения задания 1*/
+SELECT 
+  (
+    count(*) FILTER (
+      WHERE 
+        gasoline_consumption IS NULL
+    ) * 100.0 / count(*)
+  ) AS nulls_percentage_gasoline_consumption 
+FROM 
+  car_shop.cars;
