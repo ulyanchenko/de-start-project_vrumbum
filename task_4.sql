125c530ea5aa7de59f6e5c4688ff6354cd19df4d
ulyanchenko <daniil.mctracher@yandex.ru>
2024-10-28 17:30:21 +0300

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,1 +1,18 @@
 /*добавьте сюда запрос для решения задания 4*/
+SELECT 
+    c.person_name AS person,
+    STRING_AGG(DISTINCT m.name || ' ' || cm.name, ', ') AS cars
+FROM 
+    car_shop.customers c
+JOIN 
+    car_shop.sales s ON c.id = s.customer_id
+JOIN 
+    car_shop.cars car ON car.id = s.car_id
+JOIN 
+    car_shop.car_models cm ON cm.id = car.model_id
+JOIN 
+    car_shop.makes m ON m.id = cm.make_id
+GROUP BY 
+    c.person_name
+ORDER BY 
+    c.person_name ASC;
