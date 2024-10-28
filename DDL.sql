2a58227beb537496f87f25c8f83ebb5b4a10f8c4
ulyanchenko <daniil.mctracher@yandex.ru>
2024-10-28 17:33:32 +0300

    Update DDL.sql

--- DDL.sql
+++ DDL.sql
@@ -1,2 +1,22 @@
 /*Добавьте в этот файл все запросы, для создания схемы данных автосалона и
  таблиц в ней в нужном порядке*/
+CREATE SCHEMA IF NOT EXISTS raw_data;
+CREATE TABLE IF NOT EXISTS raw_data.sales(
+  id SERIAL PRIMARY KEY, 
+  auto VARCHAR(255), 
+  gasoline_consumption NUMERIC(4, 2), 
+  price NUMERIC(15, 2), 
+  sale_date DATE, 
+  person_name VARCHAR(255), 
+  phone VARCHAR(50), 
+  discount INTEGER, 
+  brand_origin VARCHAR(100)
+);
+
+COPY raw_data.sales (
+  auto, gasoline_consumption, price, 
+  sale_date, person_name, phone, discount, 
+  brand_origin
+) 
+FROM 
+  'C:/Dev/cars.csv' DELIMITER ',' CSV HEADER;
