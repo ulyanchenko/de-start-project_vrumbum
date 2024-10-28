67c55ac407114b1874b07d8fc77fd10130176a9c
ulyanchenko <daniil.mctracher@yandex.ru>
2024-10-28 22:01:47 +0300

    Update 2 DDL.sql

--- DDL.sql
+++ DDL.sql
@@ -1,47 +1,49 @@
-/*Добавьте в этот файл все запросы, для создания схемы данных автосалона и
- таблиц в ней в нужном порядке*/
 CREATE SCHEMA IF NOT EXISTS car_shop;
-
+CREATE TABLE IF NOT EXISTS car_shop.origin (
+  id SERIAL PRIMARY KEY, 
+  brand_origin VARCHAR(255)
+);
 CREATE TABLE IF NOT EXISTS car_shop.makes (
   id SERIAL PRIMARY KEY, 
-  name VARCHAR(255) NOT NULL UNIQUE
+  origin_id INT NOT NULL, 
+  name VARCHAR(255) NOT NULL UNIQUE, 
+  FOREIGN KEY (origin_id) REFERENCES car_shop.origin(id)
 );
-
 CREATE TABLE IF NOT EXISTS car_shop.car_models (
   id SERIAL PRIMARY KEY, 
   name VARCHAR(255) NOT NULL UNIQUE, 
   make_id INT NOT NULL, 
+  gasoline_consumption DECIMAL(5, 2), 
   FOREIGN KEY (make_id) REFERENCES car_shop.makes(id)
 );
-
 CREATE TABLE IF NOT EXISTS car_shop.colors (
   id SERIAL PRIMARY KEY, 
   color VARCHAR(255) NOT NULL UNIQUE
 );
-
 CREATE TABLE IF NOT EXISTS car_shop.cars (
   id SERIAL PRIMARY KEY, 
   model_id INT NOT NULL, 
-  gasoline_consumption DECIMAL(5, 2), 
-  brand_origin VARCHAR(255), 
   color_id INT NOT NULL, 
   FOREIGN KEY (model_id) REFERENCES car_shop.car_models(id), 
   FOREIGN KEY (color_id) REFERENCES car_shop.colors(id)
 );
-
 CREATE TABLE IF NOT EXISTS car_shop.customers (
   id SERIAL PRIMARY KEY, 
   person_name VARCHAR(255), 
-  phone VARCHAR(50)
+  phone VARCHAR(50) UNIQUE
 );
-
+CREATE TABLE IF NOT EXISTS car_shop.discounts (
+  id SERIAL PRIMARY KEY, 
+  discount DECIMAL(5, 2) NOT NULL
+);
 CREATE TABLE IF NOT EXISTS car_shop.sales (
   id SERIAL PRIMARY KEY, 
   car_id INT NOT NULL, 
   customer_id INT NOT NULL, 
   sale_date DATE, 
   price DECIMAL(10, 2), 
-  discount DECIMAL(5, 2) DEFAULT 0, 
+  discount_id INT, 
   FOREIGN KEY (car_id) REFERENCES car_shop.cars(id), 
-  FOREIGN KEY (customer_id) REFERENCES car_shop.customers(id)
+  FOREIGN KEY (customer_id) REFERENCES car_shop.customers(id), 
+  FOREIGN KEY (discount_id) REFERENCES car_shop.discounts(id)
 );
