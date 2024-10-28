80be9bb3cfb677e9b99f432bc0d120fd736f64b9
ulyanchenko <daniil.mctracher@yandex.ru>
2024-10-28 17:31:18 +0300

    Update task_2.sql

--- task_2.sql
+++ task_2.sql
@@ -1,1 +1,25 @@
 /*добавьте сюда запрос для решения задания 2*/
+SELECT 
+  m.name AS brand_name, 
+  EXTRACT(
+    YEAR 
+    FROM 
+      s.sale_date
+  ) AS year, 
+  ROUND(
+    AVG(
+      s.price * (1 - s.discount / 100)
+    ), 
+    2
+  ) AS price_avg 
+FROM 
+  car_shop.sales s 
+  JOIN car_shop.cars c ON c.id = s.car_id 
+  JOIN car_shop.car_models cm ON cm.id = c.model_id 
+  JOIN car_shop.makes m ON m.id = cm.make_id 
+GROUP BY 
+  m.name, 
+  year 
+ORDER BY 
+  m.name, 
+  year;
